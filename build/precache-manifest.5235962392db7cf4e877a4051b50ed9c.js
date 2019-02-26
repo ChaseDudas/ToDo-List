aa2e4ee7379d3c6b41b946b2a238b5f279915538
Chase Dudas <chasedudas13@gmail.com>
2019-02-25 22:44:56 -0700

    Added build folder for hosting to Netlify

---
+++ build/precache-manifest.5235962392db7cf4e877a4051b50ed9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1bddf159a03fb1ace704",
+    "url": "/static/js/main.1bddf159.chunk.js"
+  },
+  {
+    "revision": "4825696ee036bcfb608b",
+    "url": "/static/js/2.4825696e.chunk.js"
+  },
+  {
+    "revision": "1bddf159a03fb1ace704",
+    "url": "/static/css/main.6b1229a3.chunk.css"
+  },
+  {
+    "revision": "6017863912bf5eea87e88fcecff64256",
+    "url": "/index.html"
+  }
+];
